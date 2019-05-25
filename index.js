93ea35f7c772a0cf53442a512fe36c568b210d27
Giani Segatto <gsegatto@travix.com>
2019-05-25 17:34:25 +0100

    exporting LeapApplication

--- index.js
+++ index.js
@@ -1,9 +1,2 @@
 // "use strict";
-
-// module.exports = require("./src/main/node/functions");
-
-const LeapStarterLoader = require("./src/main/node/boot/LeapStarterLoader");
-
-const leapStarterLoader = new LeapStarterLoader();
-
-leapStarterLoader.load();
+module.exports = require("./src/main/node/functions");
